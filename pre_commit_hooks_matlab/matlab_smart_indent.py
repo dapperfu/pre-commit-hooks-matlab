fd5207f56d98033aa03671adb396c29dd93af984
Frey, Jedediah <223258+dapperfu@users.noreply.github.com>
2023-09-28 14:51:48 -0400

    Added more working code

--- pre_commit_hooks_matlab/matlab_smart_indent.py
+++ pre_commit_hooks_matlab/matlab_smart_indent.py
@@ -3,13 +3,26 @@ from typing import List
 import os
 import subprocess
 
+DEBUG=True
+
 def main(argv: List[str] = sys.argv):
+    with open("mfiles.txt", "w") as fid:
+        for mfile in argv[1:]:
+            full_path = os.path.abspath(os.getcwd(), mfile);
+            print(full_path, file=fid)
+
     with open('run_matlab_smart_indent.m', "w") as fid:
-        print(f"cd '{os.getcwd()}';", file=fid)
+        print(f"""
+cd '{os.getcwd()}';
+""", file=fid)
 
     subprocess.run(["matlab", "-batch", "run_matlab_smart_indent"])
     print(argv)
     print(os.path.dirname(os.path.realpath(__file__)))
+
+    if not DEBUG:
+        os.unlink("mfiles.txt")
+        os.unlink("run_matlab_smart_indent.m")
 
 if __name__ == "__main__":
     main()
