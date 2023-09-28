c3af0e895f08bafc9320363fecc60a527da71dd8
+++ATH0 <223258+dapperfu@users.noreply.github.com>
2023-09-27 22:04:28 -0400

    Create matlab_autoindent.m

---
+++ matlab_autoindent.m
@@ -0,0 +1,21 @@
+# ChatGPT: Write a MATLAB script to automate opening a matlab file using the editor (doc matlab.desktop.editor). Use the appropriate function to auto indent (Ctrl-I).
+
+% Define the file path
+fileToOpen = 'path/to/your/matlab/file.m';
+
+% Open the file in the MATLAB editor
+editorObj = matlab.desktop.editor.openDocument(fileToOpen);
+
+% Check if the document was opened successfully
+if ~isempty(editorObj)
+    % Perform auto-indentation (Ctrl-I)
+    editorObj.smartIndentContents();
+    
+    % Save the changes (optional)
+    % editorObj.save();
+    
+    % Close the editor (optional)
+    % editorObj.close();
+else
+    fprintf('Failed to open the file: %s\n', fileToOpen);
+end
